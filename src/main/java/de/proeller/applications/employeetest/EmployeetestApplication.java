3f068d164878c361908d1af40caa76ec18e5c868
schlottiP <charlotte.proeller@gmx.de>
2024-05-22 16:25:46 +0200

    initialize project

---
+++ src/main/java/de/proeller/applications/employeetest/EmployeetestApplication.java
@@ -0,0 +1,13 @@
+package de.proeller.applications.employeetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeetestApplication.class, args);
+	}
+
+}
